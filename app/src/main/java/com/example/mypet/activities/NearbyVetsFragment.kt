7a3d9807af9c9b7c36069f04364533a786eb8f83
Giorgos Barkos <gbarkos@arx.net>
2022-07-21 22:13:04 +0300

    changes

---
+++ app/src/main/java/com/example/mypet/activities/NearbyVetsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.mypet.activities
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+import com.example.mypet.R
+import com.google.android.libraries.places.api.Places
+
+class NearbyVetsFragment : Fragment(R.layout.fragment_nearby_vets){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/example/mypet/utils/Constants.kt
+++ app/src/main/java/com/example/mypet/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.mypet.utils
 class Constants {
     companion object {
         //private const val BASE_URL = "https://shrouded-oasis-95834.herokuapp.com/"
-        private const val BASE_URL = "http://192.168.1.6:8005/"
+        private const val BASE_URL = "http://192.168.1.7:8005/"
         private const val ACCESS_TOKEN = "ACCESS_TOKEN";
     }
 
